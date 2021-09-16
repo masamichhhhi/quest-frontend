0e3d016d99be11c649f598bbbdbcc0d200057ee4
masamichhhhi <masamichhhi@gmail.com>
2021-09-16 15:38:10 +0900

    add api

---
+++ api/go.mod
@@ -0,0 +1,20 @@
+module github.com/masamichhhhi/quest-api
+
+go 1.16
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/go-chi/chi v4.0.2+incompatible // indirect
+	github.com/go-openapi/jsonreference v0.19.6 // indirect
+	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/nicklaw5/go-respond v1.5.0 // indirect
+	github.com/swaggo/http-swagger v1.1.1
+	github.com/swaggo/swag v1.7.1 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+	golang.org/x/sys v0.0.0-20210915083310-ed5796bab164 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.5 // indirect
+)
---
+++ api/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/jinzhu/gorm"
+	_ "github.com/masamichhhhi/quest-api/docs"
+	resp "github.com/nicklaw5/go-respond"
+	httpSwagger "github.com/swaggo/http-swagger"
+)
+
+type (
+	room struct {
+		ID           uint64    `json:"id"`
+		OwnerID      uint64    `json:"owner_id"`
+		OwnerAddress string    `json:"owner_address"`
+		Title        string    `json:"title"`
+		Description  string    `json:"description"`
+		EventCode    string    `json:"event_code"`
+		Address      string    `json:"address"`
+		CreateTxHash string    `json:"create_tx_hash"`
+		IsPrivate    bool      `json:"is_private"`
+		WeiBalance   uint64    `json:"wei_balance"`
+		WeiPrize     uint64    `json:"wei_prize"`
+		StartTime    time.Time `json:"start_time"`
+		EndTime      time.Time `json:"end_time"`
+		Active       bool      `json:"active"`
+	}
+
+	rooms []room
+)
+
+var db *gorm.DB
+
+// API Documents
+// @title Sample API
+// @version 1.0.0
+func main() {
+	log.Println("Starting API Server!")
+	db = connectDB()
+	defer db.Close()
+
+	router := chi.NewRouter()
+
+	router.Get("/root", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Write([]byte("root"))
+	})
+
+	router.Get("/rooms", getRooms)
+
+	router.Get("/swagger/*", httpSwagger.WrapHandler)
+
+	http.ListenAndServe(":8080", router)
+}
+
+// @Tags Room
+// @Accept json
+// @Summary ルーム情報を取得します
+// @Success 200 {array} room
+// @Router /rooms [get]
+func getRooms(w http.ResponseWriter, r *http.Request) {
+	rooms := []room{}
+	db.Find(&rooms)
+
+	resp.NewResponse(w).Ok(rooms)
+}
+
+func connectDB() *gorm.DB {
+	DBMS := "mysql"
+	USER := "root"
+	PASS := "password"
+	PROTOCOL := "tcp(mysql)"
+	DBNAME := "test_db"
+	option := "?charset=utf8&parseTime=True"
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + option
+
+	db, err := gorm.Open(DBMS, CONNECT)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
